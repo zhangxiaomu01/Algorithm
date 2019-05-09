fd8739fc4c4ce7b717582ed84fdfa2e9b33098d2
Jun Zhang <zhangxiaomu01@users.noreply.github.com>
2019-05-09 14:13:43 -0400

    Update Backtracking.cpp

--- Backtracking.cpp
+++ Backtracking.cpp
@@ -51,4 +51,104 @@ public:
     }
 };
 
+//78. Subsets
+//https://leetcode.com/problems/subsets/
+/*
+Classic backtracking problem: Be sure to get familiar with this technique...
+The natural approach is to try all possible combination under limitations.
+*/
+class Solution {
+private:
+    void backtrack(vector<int>& n, vector<vector<int>>& res, int pos, vector<int>& temp){
+        res.push_back(temp);
+        //Note we will try all combinations
+        for(int i = pos; i < n.size(); i++){
+            temp.push_back(n[i]);
+            backtrack(n, res, i+1, temp);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int len = nums.size();
+        vector<vector<int>> res;
+        vector<int> temp;
+        backtrack(nums, res, 0, temp);
+        return res;
+    }
+};
+
+/*
+This approach is more straightforward. We mimic the process of how to build a subsets. 
+A very good approach. Neat.
+*/
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res(1, vector<int>());
+        int len = nums.size();
+        for(int i = 0; i < len; i++){
+            int n = res.size();
+            for(int j = 0; j < n; j++){
+                //We make a copy of the last element, and then insert nums[i] to the copy
+                //This is due to the nature of subsets.
+                res.push_back(res[j]);
+                res.back().push_back(nums[i]);
+            }
+        }
+        return res;
+    }
+};
+
+//90. Subsets II
+//https://leetcode.com/problems/subsets-ii/
+//The same thing like subset problem. We need to handle duplicates now
+class Solution {
+private:
+    void backtrack(vector<int>& n, vector<vector<int>>& res, int pos, vector<int>& temp){
+        res.push_back(temp);
+        for(int i = pos; i < n.size(); i++){
+            //The only duplicate we need to handle is when we do the backtrack, and encounter the same element again. 
+            //We need to get rid of the duplicate sets like this. 
+            if(i != pos && n[i] == n[i-1]) continue;
+            temp.push_back(n[i]);
+            backtrack(n, res, i+1, temp);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        //Sorting is required.
+        sort(nums.begin(), nums.end());
+        backtrack(nums, res, 0, temp);
+        return res;
+    }
+};
+//A little bit tricky....
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res(1, vector<int>());
+        int len = nums.size();
+        int n = 0;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < len; i++){
+            int start = 0;
+            //If we need to get rid of the duplicates, we only need to insert the duplicated elements to the last inserted elements. Instead of the whole elements in res.
+            //Still a little bite tricky...
+            if(i >= 1 && nums[i] == nums[i-1]) start = n;
+            else start = 0;
+            //Since we only care about the elements inserted in last round, we need to keep track of starting index of last round.
+            n = res.size();
+            for(int j = start; j < n; j++){
+                res.push_back(res[j]);
+                res.back().push_back(nums[i]);
+            }
+        }
+        return res;
+    }
+};
+
 
