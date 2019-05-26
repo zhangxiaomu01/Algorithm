f608d7ddd72e551acc37923dc4d7688b5e5ff6f3
zhangxiaomu01 <31782250+zhangxiaomu01@users.noreply.github.com>
2019-05-26 10:27:49 -0400

    Update DFS_BFS.cpp

--- DFS_BFS.cpp
+++ DFS_BFS.cpp
@@ -33,7 +33,7 @@ public:
 //130. Surrounded Regions
 //https://leetcode.com/problems/surrounded-regions/
 /*
-Wrong try... Double chek later...
+Wrong answer... I am trying to flip the 'O' in the middle of the matrix. It's hard to determine when you need to return.
 */
 class Solution {
 private:
@@ -69,5 +69,51 @@ public:
         }
     }
 };
+
+/*
+Note in this problem, we have to search from the boundary... In my first try, I try to flip the 'O' in the center, but it is very hard to determine when we need to return. 
+Then a more natural approach is to search from the boundry, and then flip the 'O' to '+', finally, we will flip all 'O' to 'X', all '+' to 'O'
+*/
+class Solution {
+private:
+    void dfs(vector<vector<char>>& B, int i, int j){
+        if(i < 0 || i >= B.size() || j < 0 || j >= B[0].size() || B[i][j] == '+')
+            return;
+        if(B[i][j] == 'O'){
+            B[i][j] = '+';
+            dfs(B, i+1, j);
+            dfs(B, i-1, j);
+            dfs(B, i, j+1);
+            dfs(B, i, j-1);
+        }
+    }
+public:
+    void solve(vector<vector<char>>& board) {
+        if(board.empty()) return;
+        int row = board.size();
+        int col = board[0].size();
+        for(int i = 0; i < row; ++i){
+            if(board[i][0] == 'O')
+                dfs(board, i, 0);
+            if(board[i][col-1])
+                dfs(board, i, col-1);
+        }
+        for(int j = 0; j < col; ++j){
+            if(board[0][j] == 'O')
+                dfs(board, 0, j);
+            if(board[row-1][j] == 'O')
+                dfs(board, row-1, j);
+        }
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if(board[i][j] == '+')
+                    board[i][j] = 'O';
+            } 
+        }
+    }
+};
+
 
 
