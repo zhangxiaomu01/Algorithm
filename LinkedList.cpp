d63b6259986a9898da27e9a9692ead756b3fe8a1
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-07-24 16:50:03 -0400

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -60,3 +60,33 @@ public:
 };
 
 
+//21. Merge Two Sorted Lists
+//
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 && !l2) return l1;
+        else if (l2 && !l1) return l2;
+        else if(!l1 && !l2) return nullptr;
+        
+        ListNode* dummy = new ListNode(0);
+        ListNode* pre = dummy;
+            
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                pre->next = l1;
+                pre = pre->next;
+                l1 = l1->next;
+            }else{
+                pre->next = l2;
+                pre = pre->next;
+                l2 = l2->next;
+            }
+        }
+        if(l1) pre->next = l1;
+        else if(l2) pre->next = l2;
+        ListNode* ptr = dummy->next;
+        delete dummy;
+        return ptr;
+    }
+};
