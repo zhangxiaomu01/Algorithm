8f1f9a4cdce045bb694a50fae3fb25de9028951f
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-07-24 17:35:36 -0400

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -30,3 +30,33 @@ public:
 };
 
 
+//3. Longest Substring Without Repeating Characters
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+//The general idea is sliding window. We use hash map to store
+//the visited element. Each time when we encounter the duplicate 
+//element, we slide the left pointer and until we reach the new 
+//duplicate element. Each loop we update the max length and save the 
+//visited element to the map
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int len = s.size();
+        unordered_map<char, int> map;
+        int maxLen = 0;
+        int start = 0;
+        for(int i = 0; i < len; ++i){
+            if(map.find(s[i])!= map.end()){
+                //Slide the lowest boundry
+                while(map.count(s[i]) > 0) map.erase(s[start++]);
+            }
+            //Note we shift the start 1 character right
+            maxLen = max(maxLen, i - start + 1);
+            map[s[i]] = i;
+        }
+        return maxLen;
+    }
+};
+
+
+
+
