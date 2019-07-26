c00ab9bd06199dcbcb356322fb6ef773a8048320
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-07-26 15:28:05 -0400

    Update DP.cpp

--- DP.cpp
+++ DP.cpp
@@ -484,3 +484,30 @@ public:
 };
 
 
+//64. Minimum Path Sum
+//https://leetcode.com/problems/minimum-path-sum/
+/* 2D DP: MN matrix, can optimize to 1D array */
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = m ? grid[0].size() : 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[m-1][n-1] = grid[m-1][n-1];
+        for(int i = m-1; i >= 0; --i){
+            for(int j = n-1; j >= 0; --j){
+                if(i == m-1 && j == n-1) continue;
+                if(i == m-1){
+                    dp[i][j] = dp[i][j+1] + grid[i][j];
+                }else if(j == n-1){
+                    dp[i][j] = dp[i+1][j] + grid[i][j];
+                }else{
+                    dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+//
