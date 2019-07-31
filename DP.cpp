5a36fc35ef119721b23fcfd97c073645d90bab79
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-07-31 16:33:48 -0400

    Update DP.cpp

--- DP.cpp
+++ DP.cpp
@@ -1303,5 +1303,24 @@ public:
     }
 };
 
+/* Lower bound solution: O(nlogn) */
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int len = nums.size();
+        vector<int> res;
+        for(int i = 0; i < len; ++i){
+            auto it = lower_bound(res.begin(), res.end(), nums[i]);
+            if(it == res.end())
+                res.push_back(nums[i]);
+            else 
+                *it = nums[i];
+        }
+        return res.size();
+    }
+};
+
+
 
 
