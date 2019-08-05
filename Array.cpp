b615e6e963944678217c85d5643d7a0b0951b954
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-08-05 16:58:23 -0400

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1933,4 +1933,13 @@ public:
 };
 
 
+//88. Merge Sorted Array
+//https://leetcode.com/problems/merge-sorted-array/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        while(n)
+            nums1[m+n-1] = (m > 0 && nums1[m-1] > nums2[n-1])? nums1[--m] : nums2[--n];
+    }
+};
 
