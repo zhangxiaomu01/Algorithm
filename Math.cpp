d9c418a18434554bdc11186a3b5f8bf2405f9a78
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-08-05 12:49:52 -0400

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -148,4 +148,19 @@ public:
     }
 };
 
+//Newton's law: https://en.wikipedia.org/wiki/Newton%27s_method
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num <= 0) return false;
+        if(num == 1) return true;
+        long t = num / 2;
+        while(t * t > num){
+            t = (t + num / t)/ 2;
+        }
+        return t * t == num;
+    }
+};
+
+
 
