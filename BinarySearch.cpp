2395ae7d22e86b0d2c3a0da95ba7cea1ddb91218
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-08-10 15:38:29 -0400

    Update

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -26,3 +26,26 @@ public:
     }
 };
 
+
+//278. First Bad Version
+//https://leetcode.com/problems/first-bad-version/
+/* Standard binary search */
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l = 1, r = n;
+        while(l < r){
+            int mid = l + (r - l) / 2;
+            if(isBadVersion(mid))
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+};
+
+
