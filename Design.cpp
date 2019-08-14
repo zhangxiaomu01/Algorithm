974c335ccdb2be5d316c9bb57a73b9c2f4f5e6da
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-08-14 14:33:10 -0400

    Update Design.cpp

--- Design.cpp
+++ Design.cpp
@@ -462,6 +462,95 @@ public:
 
 
 
+//382. Linked List Random Node
+//https://leetcode.com/problems/linked-list-random-node/
+/*
+A good problem to refresh random library. Then default implementation is trivial.
+Try follow up.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    int m_size;
+    ListNode* m_head;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        m_head = head;
+        m_size = 0;
+        while(head){
+            m_size++;
+            head = head -> next;
+        }
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        default_random_engine seed((random_device())());
+        uniform_int_distribution<int> distriIndex(0, m_size - 1);
+        int pos = distriIndex(seed);
+        ListNode* ptr = m_head;
+        while(pos > 0){
+            ptr = ptr->next;
+            pos --;
+        }
+        return ptr->val;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
+
+//Follow up implementation: Now list is large, you cannot know its size
+//Reservoir sampling algorithm!!
+//https://www.youtube.com/watch?time_continue=1&v=A1iwzSew5QY
+class Solution {
+private:
+    ListNode* m_head;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        m_head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int reservoirNum = 0;
+        default_random_engine seed((random_device())());
+        ListNode* ptr = m_head;
+        int res = 0;
+        while(ptr){
+            uniform_int_distribution distr(0, reservoirNum);
+            int index = distr(seed);
+            if(index == 0)
+                res = ptr->val;
+            reservoirNum++;
+            ptr = ptr->next;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
+
+
+
 
 
 
