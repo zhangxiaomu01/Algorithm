5a0c8ddbb48ef2a4c59c33913ddee0bef42b5263
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-08-15 20:57:40 -0400

    Update DFS_BFS.cpp

--- DFS_BFS.cpp
+++ DFS_BFS.cpp
@@ -443,3 +443,34 @@ public:
 };
 
 
+//The most updated version, however, it's almost impossible to get it during the 
+//interview
+class Solution 
+{
+    int res = 0;
+public:
+    int integerReplacement(int n) 
+    {
+        if (n == 1)
+            return res;
+        if (n == 3)
+        {
+            res += 2;
+            return res;
+        }
+        if (n == INT_MAX)
+            return 32;
+        
+        res ++;
+        if (n & 1)
+            if ((n + 1) % 4 == 0)
+                integerReplacement(n + 1);
+            else
+                integerReplacement(n - 1);
+        else
+            integerReplacement(n / 2);
+            
+        return res;
+    }
+};
+
