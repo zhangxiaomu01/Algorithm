9c323a86158848a0bccd00acfdce8e69696691ac
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-08-25 21:44:38 -0400

    Update Design.cpp

--- Design.cpp
+++ Design.cpp
@@ -1083,3 +1083,61 @@ public:
  * obj->unfollow(followerId,followeeId);
  */
 
+//More optimized version, never implement by myself. I think the reason why it
+//is faster is because getNewsFeed() function does the pruning before hand. In
+//my implementation, I just search the list from front to end, which may involves
+//many unnecessary check.
+class Twitter {
+public:
+    /** Initialize your data structure here. */
+    unordered_map<int, set<int> > followers;
+    unordered_map<int, vector<pair<int, int>  > > users;
+    int time = 1;
+    Twitter() {
+        
+    }
+    
+    /** Compose a new tweet. */
+    void postTweet(int userId, int tweetId) {
+        users[userId].push_back({time++,tweetId});
+    }
+    
+    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+    vector<int> getNewsFeed(int userId) {
+        priority_queue<pair<int, int> > pq; 
+        vector<pair<int, int> > userTweets = users[userId];
+        for(int i=userTweets.size()-1, c=0; c<10 && i>=0; i--, c++)
+        {
+            pq.push(userTweets[i]);
+        }
+        set<int> follow = followers[userId];
+        for(auto it: follow)
+        {
+            vector<pair<int, int> > followTweets = users[it];
+            for(int j=followTweets.size()-1, c=0; c<10 && j>=0; j--, c++)
+            {
+                pq.push(followTweets[j]);
+            }
+        }
+        
+        vector<int> ans;
+        for(int i=0; i<10 && !pq.empty(); i++)
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+    
+    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+    void follow(int followerId, int followeeId) {
+        if(followerId != followeeId)
+          followers[followerId].insert(followeeId);
+    }
+    
+    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+    void unfollow(int followerId, int followeeId) {
+        followers[followerId].erase(followeeId);
+    }
+};
+
