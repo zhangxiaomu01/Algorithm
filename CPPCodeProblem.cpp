f827580025cb305557bb556c2c5cb99f22909e9c
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-08-27 19:50:10 -0400

    Update CPPCodeProblem.cpp

--- CPPCodeProblem.cpp
+++ CPPCodeProblem.cpp
@@ -396,5 +396,207 @@ public:
 };
 
 
+//////////////////////////////////////////////////////////////////////////////
+//Design cash register program
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+#include<iterator>
+#include<sstream>
+
+/*
+Recursive solution + memorization
+*/
+
+class CashRegister
+{
+public:
+	CashRegister() = default;
+	CashRegister(const std::vector<int>& num) {
+		
+		for (int i = 0;i < num.size(); ++i) {
+			mTill[denomination(enumMaps[i])] = num[i];
+		}
+		//Initialize total money
+		m_totalMoney = static_cast<int> (TotalInRegister() * 100);
+	};
+	~CashRegister() {};
+
+	// the customer has paid money,
+	// that money is already in the till
+	// Now, dispense change
+	//Assume the amountPaid and amountOwed represents dollars (not cents)
+	void MakeChange(double amountPaid, double amountOwed);
+
+private:
+	// the cash register holds zero or more of these
+	// bills and coins in the Till.
+	// The value of the enum is its worth in cents
+	enum denomination
+	{
+		kPenny = 1,
+		kNickel = 5,
+		kDime = 10,
+		kQuarter = 25,
+		kHalf = 50,
+		kDollar = 100,
+		kFive = 500,
+		kTen = 1000,
+		kTwenty = 2000,
+		kFifty = 5000,
+		kHundred = 10000
+	};
+
+
+	// how much money is in the cash register?
+	double TotalInRegister() const;
+
+	// remove coins/bills from the till and give them to the customer 
+	//--------------------Did not use it in my code --------------------------
+	void Dispense(denomination d, int count);
+
+	// there is a problem!
+	void ReportError(const char *text) const;
+
+	//Recursive helper function
+	int helper(std::vector<std::vector<int>>& memo, int amount, int pos);
+
+private:
+
+	// This is the till.  All bills and coins are stored here
+	std::map< denomination, int > mTill;
+
+	// This is the LCD display on the cash register
+	std::ostringstream mDisplay;
+
+	//The totalNumber of money (cents). 
+	//The range may not be sufficient for large transaction!
+	int m_totalMoney;
+	
+	//A map between enum and array
+	int enumMaps[11] = { 1, 5, 10, 25, 50, 100, 500, 1000, 2000, 5000, 10000 };
+
+};
+
+// -------------------------------------------------------
+// Function:    CashRegister::TotalInRegister
+// Purpose:     how much money is in the cash register?
+double CashRegister::TotalInRegister() const
+{
+	int total(0);
+	auto it = mTill.begin();
+	for (; it != mTill.end(); it++)
+		total += ((int)it->first) * it->second;
+	std::cout << "Total maoney in register: " << total / 100.0 << " dollars" << std::endl;
+	return total / 100.0;
+}
+
+// -------------------------------------------------------
+// Function:    CashRegister::Dispense
+// Purpose:     remove coins/bills from the till and give them to the customer
+
+void CashRegister::Dispense(denomination d, int count)
+{
+	mTill[d] -= count;
+}
+
+// -------------------------------------------------------
+// Function:    CashRegister::ReportError
+// Purpose:     there is a problem!
+
+void CashRegister::ReportError(const char *text) const
+{
+	// show the problem on the display 
+	//mDisplay << text;
+	std::cout << text << std::endl;
+}
+
+// -------------------------------------------------------
+
+// ******************************
+//     <insert your code here>
+// ******************************
+
+int CashRegister::helper(std::vector<std::vector<int>>& memo, int amount, int pos) {
+	//int enumMaps[11] = { 1, 5, 10, 25, 50, 100, 500, 1000, 2000, 5000, 10000 };
+	if (amount == 0) return 1;
+	if (amount < 0 || pos < 0) return 0;
+	if (memo[amount][pos] != -1) return memo[amount][pos];
+	//always start from the largest possible money
+	for (int i = pos; i >= 0; --i) {
+		denomination d = denomination(enumMaps[i]);
+		int numOfCoins = mTill[denomination(enumMaps[i])];
+		int coinDenomination = enumMaps[i];
+		if (numOfCoins > 0 && coinDenomination <= amount) {
+			mTill[d]--;
+			if (helper(memo, amount - coinDenomination, i) == 1) {
+				memo[amount][pos] = 1;
+				return memo[amount][pos];
+			}
+			mTill[d]++;
+		}
+	}
+	memo[amount][pos] = 0;
+	return memo[amount][pos];
+}
+
+void CashRegister::MakeChange(double amountPaid, double amountOwed)
+{
+	int difference = static_cast<int> (amountPaid * 100.0 - amountOwed * 100.0);
+
+	const char* msg = "No sufficient funds in the register. ";
+	if(difference > m_totalMoney){
+		ReportError(msg);
+		return;
+	}
+
+	std::vector<std::vector<int>> memo(difference+1, std::vector<int>(11, -1));
+	//Make a copy of the mTill, so we can track which coin we need to pick up later
+	std::map< denomination, int > coinsLeft(mTill.begin(), mTill.end());
+
+	if (helper(memo, difference, 10)) {
+		std::vector<int> coinsToPick(11, 0);
+		int i = 0;
+		auto coinsLeftIt = coinsLeft.begin();
+		for (auto it = mTill.begin(); it != mTill.end(); ++it) {
+			coinsToPick[i++] = coinsLeftIt->second - it->second;
+			coinsLeftIt++;
+		}
+		//May be better to wrap in the function, print one possible choice
+		std::cout << "One of the possible choice for the change is:" << std::endl;
+		std::cout << "kPenny:   " << coinsToPick[0] << std::endl;
+		std::cout << "kNickel:  " << coinsToPick[1] << std::endl;
+		std::cout << "kDime:    " << coinsToPick[2] << std::endl;
+		std::cout << "kQuarter: " << coinsToPick[3] << std::endl;
+		std::cout << "kHalf:    " << coinsToPick[4] << std::endl;
+		std::cout << "kDollar:  " << coinsToPick[5] << std::endl;
+		std::cout << "kFive:    " << coinsToPick[6] << std::endl;
+		std::cout << "kTen:     " << coinsToPick[7] << std::endl;
+		std::cout << "kTwenty:  " << coinsToPick[8] << std::endl;
+		std::cout << "kFifty:   " << coinsToPick[9] << std::endl;
+		std::cout << "kHundred: " << coinsToPick[10] << std::endl;
+	}
+	else {
+		ReportError("Impossible to make the change! \n");
+	}
+	m_totalMoney = static_cast<int>(TotalInRegister());
+	
+}
+
+
+int main() {
+	int enumMaps[11] = { 1, 5, 10, 25, 50, 100, 500, 1000, 2000, 5000, 10000 };
+	std::vector<int> numOfCoins = { 3,11,23,4,12,6,9,4,7,12,1 };
+	CashRegister cR(numOfCoins);
+	//Assume the amountPaid and amountOwed represents dollars (not cents)
+	//We will modify the mTill when we call the function
+	cR.MakeChange(19.73, 14.28); //Good
+	//cR.MakeChange(50.05, 0.01);//impossible to make the change
+	//cR.MakeChange(1000.0, 14.28);//No sufficient funds
+
+	system("pause");
+	return 0;
+}
 
 
