808ff8fd9c335437376b08ecd67e6de1fbcfde4e
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-09-16 16:17:03 -0400

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -383,6 +383,39 @@ public:
 
 //240. Search a 2D Matrix II
 //https://leetcode.com/problems/search-a-2d-matrix-ii/
+//The most natural approach! binary search for each row
+class Solution {
+    bool searchVector(const vector<int>& v, int target){
+        int len = v.size();
+        int l = 0, r = len-1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(v[mid] == target) return true;
+            else if (v[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return false;
+    }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = m ? matrix[0].size() : 0;
+        if(!m || !n) return false;
+        for(int i = 0; i < m; ++i){
+            if(target == matrix[i].front() || target == matrix[i].back())
+                return true;
+            if(target < matrix[i].front())
+                return false;
+            if(target > matrix[i].back())
+                continue;
+            if(searchVector(matrix[i], target)) return true;
+        }
+        return false;
+    }
+};
+
+//
+
 /*
 A natura approach is to iterate through each row, and do the binary search, the 
 time complexity should be O(m log n), we can potentially have some optimization 
