8876844cb2113764e84aa7c1e339d75ab27e0265
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-09-17 16:04:39 -0400

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -1738,3 +1738,61 @@ public:
 };
 
 
+
+//680. Valid Palindrome II
+//https://leetcode.com/problems/valid-palindrome-ii/
+//Not hard, check skip which character!
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int len = s.size();
+        int i = 0, j = len - 1;
+        while(i < j){
+            if(s[i] == s[j]){
+                i++; j--;
+            }else
+                break;
+        }
+        if(i >= j) return true;
+        
+        if(i < j){
+            int curI = i, curJ = j;
+            i++;
+            while(i < j){
+                if(s[i] == s[j]){
+                    i++; 
+                    j--;
+                }  
+                else 
+                    break;
+            }
+            if(i >= j) return true;
+            i = curI;
+            j = curJ;
+            j--;
+            while(i < j){
+                if(s[i] == s[j]){
+                    i++; j--;
+                } 
+                else 
+                    break;
+            }
+            if(i >= j) return true;
+        }
+        return false;
+    }
+};
+
+//More concise version
+bool validPalindrome(string s) {
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--)
+        if (s[i] != s[j]) {
+            int i1 = i, j1 = j - 1, i2 = i + 1, j2 = j;
+            while (i1 < j1 && s[i1] == s[j1]) {i1++; j1--;};
+            while (i2 < j2 && s[i2] == s[j2]) {i2++; j2--;};
+            return i1 >= j1 || i2 >= j2;
+        }
+    return true;
+}
+
+
