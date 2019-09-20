744900f0ee747c96fb0115a3de114fafe3010fe3
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-09-20 14:10:36 -0400

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -4065,3 +4065,40 @@ public:
     }
 };
 
+
+//406. Queue Reconstruction by Height
+//https://leetcode.com/problems/queue-reconstruction-by-height/
+/*
+//https://leetcode.com/problems/queue-reconstruction-by-height/discuss/
+Pick out tallest group of people and sort them in a subarray (S). Since there's no other groups of people taller than them, therefore each guy's index will be just as same as his k value.
+For 2nd tallest group (and the rest), insert each one of them into (S) by k value. So on and so forth.
+
+E.g.
+input: [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+subarray after step 1: [[7,0], [7,1]]
+subarray after step 2: [[7,0], [6,1], [7,1]]
+
+Very tricky problem!
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        //Note we will put people with higher height and fewer k to the 
+        //front area of our array
+        //For the example input, after sort:
+        //[[7,0], [7,1], [6,1], [5,0], [5,2], [4,4]]
+        sort(people.begin(), people.end(), [](vector<int>& v1, vector<int>& v2){ return v1[0] > v2[0] || (v1[0] == v2[0] && v1[1] < v2[1]); });
+        //Note we cannot use list<vector<int>> here because list only 
+        //supports bidirectional iterator, we cannot have list.begin()+k
+        vector<vector<int>> res;
+        for(auto& p : people){
+            res.insert(res.begin() + p[1], p);
+        }
+        return res;
+    }
+};
+
+
+
