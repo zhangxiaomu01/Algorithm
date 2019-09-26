519a533d03373d5bb7fa71a404341a5e59a20630
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-09-25 22:56:53 -0400

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -2480,3 +2480,23 @@ public:
     }
 };
 
+
+//125. Valid Palindrome
+//https://leetcode.com/problems/valid-palindrome/
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.empty()) return true;
+        int i = 0, j = s.size()-1;
+        while(i < j){
+            while(isalnum(s[i]) == 0 && i < j) i++;
+            while(isalnum(s[j]) == 0 && i < j) j--;
+            if(toupper(s[i]) != toupper(s[j])) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
+
+
