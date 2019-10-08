1889bbd0d7e451d72f83159c989529d34373cb39
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-10-07 21:46:03 -0400

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -981,6 +981,37 @@ public:
     }
 };
 
+//Divid and conquer approach!
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArray(nums, 0, nums.size() - 1);
+    }
+private:
+    int maxSubArray(vector<int>& nums, int l, int r) {
+        if (l > r) {
+            return INT_MIN;
+        }
+        int m = l + (r - l) / 2, ml = 0, mr = 0;
+        //max subarray sum appears to be in left half
+        int lmax = maxSubArray(nums, l, m - 1);
+        //max subarray sum appears to be in right half
+        int rmax = maxSubArray(nums, m + 1, r);
+        
+        //max subarray sum appears to be in both halves
+        for (int i = m - 1, sum = 0; i >= l; i--) {
+            sum += nums[i];
+            ml = max(sum, ml);
+        }
+        for (int i = m + 1, sum = 0; i <= r; i++) {
+            sum += nums[i];
+            mr = max(sum, mr);
+        }
+        //Combine the merge together
+        return max(max(lmax, rmax), ml + mr + nums[m]);
+    }
+};
+
 
 //309. Best Time to Buy and Sell Stock with Cooldown
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
