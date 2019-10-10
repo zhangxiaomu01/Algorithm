5daa4cab43a6b870b6dc2145e6f32636d6e5043c
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-10-09 21:22:44 -0400

    Update Multithreading.cpp

--- Multithreading.cpp
+++ Multithreading.cpp
@@ -87,3 +87,54 @@ public:
 
 
 
+//1115. Print FooBar Alternately
+//https://leetcode.com/problems/print-foobar-alternately/
+class FooBar {
+private:
+    int n;
+    mutex mu;
+    condition_variable cv;
+    bool isFoo;
+
+public:
+    FooBar(int n) {
+        this->n = n;
+        isFoo = false;
+    }
+
+    void foo(function<void()> printFoo) {
+        //we cannot put unique_lock here, or when the function call ends
+        //we will potentially unlock locker twice!
+        //unique_lock<mutex> locker(mu);
+        
+        for (int i = 0; i < n; i++) {
+            //Define locker here, note when we notify other's we set the 
+            //state to be another one
+            unique_lock<mutex> locker(mu);
+            while(isFoo){
+                cv.wait(locker);
+            }
+            // printFoo() outputs "foo". Do not change or remove this line.
+            printFoo();
+            isFoo = true;
+            cv.notify_one();
+            
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        for (int i = 0; i < n; i++) {
+            
+            unique_lock<mutex> locker(mu);
+            while(!isFoo){
+                cv.wait(locker);
+            }
+            // printBar() outputs "bar". Do not change or remove this line.
+            printBar();
+            isFoo = false;
+            
+            cv.notify_one();
+            
+        }
+    }
+};
