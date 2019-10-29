8b1d3093a681b66843fd7f044dd5112c549fb07c
zhangxiaomu01 <zhangxiaomu01@gmail.com>
2019-10-29 16:06:47 -0400

    Update DFS_BFS.cpp

--- DFS_BFS.cpp
+++ DFS_BFS.cpp
@@ -1624,3 +1624,39 @@ public:
         return N % 2 == 0;
     }
 };
+
+
+//286. Walls and Gates
+//https://leetcode.com/problems/walls-and-gates/
+//Standard BFS with some variations.
+//Note we can start from all gates, and keep searching their neighbours. 
+//Until all the INF grid has been updated.
+//O(mn)
+class Solution {
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        const int row = rooms.size();
+        if (0 == row) return;
+        const int col = rooms[0].size();
+        queue<pair<int, int>> canReach;  // save all element reachable
+        vector<pair<int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // four directions for each reachable
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(0 == rooms[i][j])
+                    canReach.emplace(i, j);
+            }
+        }
+        while(!canReach.empty()){
+            int r = canReach.front().first, c = canReach.front().second;
+            canReach.pop();
+            for (auto dir : dirs) {
+                int x = r + dir.first,  y = c + dir.second;
+                // if x y out of range or it is obstasle, or has small distance aready
+                if (x < 0 || y < 0 || x >= row || y >= col || rooms[x][y] <= rooms[r][c]+1) continue;
+                rooms[x][y] = rooms[r][c] + 1;
+                canReach.emplace(x, y);
+            }
+        }
+    }
+};
+
