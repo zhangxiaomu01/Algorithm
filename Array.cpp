a2b67e141a848b6aee43340c62b1cb0c3ae8b2e3
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2021-05-11 21:22:51 -0700

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -7315,3 +7315,92 @@ public:
         return res;
     }
 };
+
+
+
+// 1856. Maximum Subarray Min-Product (***)
+// https://leetcode.com/problems/maximum-subarray-min-product/
+/* A very classic problem for how to handling the non-decreasing stack.
+Solution: https://leetcode.com/problems/maximum-subarray-min-product/discuss/1198896/O(n)-Monostack-with-picture
+Like this question.
+Very important question!
+*/
+class Solution {
+public:
+    int maxSumMinProduct(vector<int>& nums) {
+        const int MOD = 1e9 + 7;
+        int len = nums.size();
+        long res = 0;
+        vector<int> st; // Stores the index of the maximum elements
+        vector<long> sums(len + 1, 0);
+        for (int i = 1; i <= len; ++i) {
+            sums[i] = sums[i-1] + nums[i-1];
+        }
+        
+        for(int i = 0; i <= len; ++i) {
+            while(!st.empty() && (i == len || nums[st.back()] > nums[i])) {
+                // previous larger element.
+                int j = st.back();
+                st.pop_back();
+                // We need to add + 1 here because st.back() will be the smaller than nums[j]
+                // In this step, we are examing the subsequence which includes nums[j] as the smallest
+                res = max(res, long(nums[j] * (sums[i] - sums[st.empty() ? 0 : st.back() + 1])));
+            }
+            st.push_back(i);
+        }
+        return res % MOD;
+    }
+};
+
+
+// Another more intuitive but slower implementation:
+// https://leetcode.com/problems/maximum-subarray-min-product/discuss/1198708/C%2B%2B-simple-solution-using-stacks-oror-Explained!!
+/* A very classic problem for how to handling the non-decreasing stack.
+Solution: https://leetcode.com/problems/maximum-subarray-min-product/discuss/1198896/O(n)-Monostack-with-picture
+Like this question.
+Very important question!
+*/
+class Solution {
+public:
+    int maxSumMinProduct(vector<int>& nums) {
+        int n = nums.size();
+        int left[n], right[n];
+        long long int sum[n];
+        sum[0] = nums[0];
+        for(int i=1;i<n;i++){
+            sum[i] = sum[i-1]+nums[i];
+        }
+        stack<pair<int, int> > s1, s2;
+        for (int i = 0; i < n; ++i) {
+            int cnt = 1;
+            while (!s1.empty() && (s1.top().first) > nums[i]) {
+                cnt += s1.top().second;
+                s1.pop();
+            }
+            s1.push({ nums[i], cnt });
+            left[i] = cnt;
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            int cnt = 1;
+            while (!s2.empty() && (s2.top().first) >= nums[i]) {
+                cnt += s2.top().second;
+                s2.pop();
+            }
+
+            s2.push({ nums[i], cnt });
+            right[i] = cnt;
+        }
+        long long int res =0;
+        for(int i=0;i<n;i++){
+            int a = i-left[i]+1;
+            int b = i+right[i]-1;
+            long long int subArraySum;
+            if(a>0)
+                subArraySum = sum[b]-sum[a-1];
+            else
+                subArraySum = sum[b];
+            res = max(res, subArraySum*nums[i]);
+        }
+        return res%1000000007;
+    }
+};
