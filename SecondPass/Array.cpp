af8957ed8e56829b715fd7897708a2f58cf5d66a
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2022-05-22 09:32:09 -0700

    Adds problem 69 to the code base.

--- SecondPass/Array.cpp
+++ SecondPass/Array.cpp
@@ -137,3 +137,42 @@ public:
         return res;
     }
 };
+
+ /*
+    69. Sqrt(x)
+    https://leetcode.com/problems/sqrtx/
+ 
+    Given a non-negative integer x, compute and return the square root of x.
+
+    Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+    Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+    
+
+    Example 1:
+
+    Input: x = 4
+    Output: 2
+    Example 2:
+
+    Input: x = 8
+    Output: 2
+    Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+ */
+// Solution: binary search.
+class Solution {
+public:
+    int mySqrt(int x) {
+        long l = 0, r = x;
+        long m = 0;
+        while (l < r) {
+            m = l + (r - l) / 2;
+            if (m * m == x) return m;
+            else if (m * m < x) l = m + 1;
+            else r = m - 1;
+        }
+        if (l * l > x) l = l - 1;
+        return l;
+    }
+};
--- SecondPass/README.md
+++ SecondPass/README.md
@@ -6,4 +6,5 @@ ID | Name | Difficulty | Category
 704|[Binary Search](https://leetcode.com/problems/binary-search/)| Easy | Binary Search
 35|[Search Insert Position](https://leetcode.com/problems/search-insert-position/)| Easy | Binary Search
 34|[Find First and Last Position of Element in Sorted Array](https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/)| Medium | Binary Search
+69|[Sqrt(x)](https://leetcode.com/problems/sqrtx/)| Easy | Binary Search
 
