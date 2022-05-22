0d113c0e8d403af126e0dbe771dc8da6e8235e82
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2022-05-21 22:20:35 -0700

    Adds problem 34 to the code base (binary search).

--- SecondPass/Array.cpp
+++ SecondPass/Array.cpp
@@ -84,4 +84,56 @@ public:
     }
 };
 
+ /*
+    34. Find First and Last Position of Element in Sorted Array
+    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+ 
+    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 
+    If target is not found in the array, return [-1, -1].
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+    Example 1:
+
+    Input: nums = [5,7,7,8,8,10], target = 8
+    Output: [3,4]
+
+    Example 2:
+
+    Input: nums = [5,7,7,8,8,10], target = 6
+    Output: [-1,-1]
+ */
+// Solution 01: two pass iterative binary search
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int m = 0;
+        vector<int> res (2, -1);
+        while (l <= r) {
+            m = l + (r - l) / 2;
+            if (nums[m] < target) l = m + 1;
+            else if (nums[m] > target) r = m - 1;
+            else {
+                res[0] = m;
+                r = m -1;
+            }
+        }
+        // No target found, early return.
+        if (res[0] == -1) return res;
+        
+        l = 0;
+        r = nums.size() - 1;
+        while (l <= r) {
+            m = l + (r - l) / 2;
+            if (nums[m] < target) l = m + 1;
+            else if (nums[m] > target) r = m - 1;
+            else {
+                res[1] = m;
+                l = m + 1;
+            }
+        }
+        return res;
+    }
+};
