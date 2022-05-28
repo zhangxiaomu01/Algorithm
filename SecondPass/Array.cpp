06936ebabc1d6fd6404f64a8bf30cc6aa1dde28d
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2022-05-28 16:42:28 -0700

    Adds a new approach for problem 27

--- SecondPass/Array.cpp
+++ SecondPass/Array.cpp
@@ -289,3 +289,16 @@ public:
     }
 };
 
+// Fast + slow pointer: similar approach
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.size(); fastIndex++) {
+            if (val != nums[fastIndex]) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+};
