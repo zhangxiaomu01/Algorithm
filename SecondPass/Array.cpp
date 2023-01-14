088d91fa107ef7d77acd2e5959c736ce564bac31
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2023-01-14 15:58:32 -0800

    Adds problem 906 to the list

--- SecondPass/Array.cpp
+++ SecondPass/Array.cpp
@@ -653,3 +653,97 @@ public:
     }
 };
 
+ /*
+    904. Fruit Into Baskets
+    https://leetcode.com/problems/fruit-into-baskets/description/
+ 
+    You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+
+    You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+    You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+    Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+    Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+    Given the integer array fruits, return the maximum number of fruits you can pick.
+
+    Example 1:
+
+    Input: fruits = [1,2,1]
+    Output: 3
+    Explanation: We can pick from all 3 trees.
+    Example 2:
+
+    Input: fruits = [0,1,2,2]
+    Output: 3
+    Explanation: We can pick from trees [1,2,2].
+    If we had started at the first tree, we would only pick from trees [0,1].
+    Example 3:
+
+    Input: fruits = [1,2,3,2,2]
+    Output: 4
+    Explanation: We can pick from trees [2,3,2,2].
+    If we had started at the first tree, we would only pick from trees [1,2].
+    
+
+    Constraints:
+
+    1 <= fruits.length <= 105
+    0 <= fruits[i] < fruits.length
+ */
+// My solution:
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        map<int, int> basket;
+        basket[fruits[0]] = 1;
+        int finalMax = 1;
+        int runningMax = 1;
+        int i = 0;
+        for(int j = 1; j < fruits.size(); ++j) {
+            if (basket.find(fruits[j]) != basket.end() || (basket.find(fruits[j]) == basket.end() && basket.size() < 2) ) {
+                basket[fruits[j]] ++;
+                runningMax ++;
+                finalMax = max(finalMax, runningMax);
+            } else if (basket.size() == 2 && basket.find(fruits[j]) == basket.end()) {
+                while (i < j) {
+                    runningMax --;
+                    basket[fruits[i]]--;
+                    if (basket[fruits[i]] == 0) { 
+                        basket.erase(fruits[i]);
+                        i++;
+                        break;
+                    }
+                    i++;
+                }
+                basket[fruits[j]] ++;
+                runningMax ++;
+                finalMax = max(finalMax, runningMax);
+            }
+        }
+        return finalMax;
+    }
+};
+
+// More elegant and trickier impl
+//Sliding window. The first pointer always includes new elements
+//When our basket is full, we need to move the second pointer 
+//forward. In general, the time complexity is O(n)
+//Exactly the same idea! However, this implementation is elegant!
+//Note we do not need to move i to the designated position and 
+//update each time, since i and j move with the same speed, we can
+//update i only when count.size() > 2
+class Solution {
+public:
+    int totalFruit(vector<int> &tree) {
+        unordered_map<int, int> count;
+        int i, j;
+        for (i = 0, j = 0; j < tree.size(); ++j) {
+            count[tree[j]]++;
+            if (count.size() > 2) {
+                if (--count[tree[i]] == 0)count.erase(tree[i]);
+                i++;
+            }
+        }
+        return j - i;
+    }
+};
