35c5ba9e38bd7b63fc2f8b5c7298712281503193
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2023-01-15 17:26:31 -0800

    Adds the problem 206 to the list.

--- SecondPass/LinkedList.cpp
+++ SecondPass/LinkedList.cpp
@@ -223,3 +223,58 @@ public:
         }
     }
 };
+
+/*
+    206. Reverse Linked List
+    https://leetcode.com/problems/reverse-linked-list/
+ 
+    Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+    Example 1:
+    Input: head = [1,2,3,4,5]
+    Output: [5,4,3,2,1]
+
+    Example 2:
+    Input: head = [1,2]
+    Output: [2,1]
+
+    Example 3:
+    Input: head = []
+    Output: []
+
+    Constraints:
+    The number of nodes in the list is the range [0, 5000].
+    -5000 <= Node.val <= 5000
+
+    Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+// Iterative
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* p = reverseList(head->next);
+        ListNode* temp = head->next;
+        head->next = nullptr;
+        temp->next = head;
+        return p;
+    }
+};
+
+// Recursive
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode* reversedList = reverseList(head->next);
+        ListNode* ptr = reversedList;
+        // The next 2 lines are the brueforce solution, we can easily retrieve the last node of the reversed list
+        // with head->next;
+        // while(reversedList->next) reversedList = reversedList->next;
+        // reverseList->next = head;
+        ListNode* temp = head->next;
+        temp->next = head;
+        head->next = nullptr;
+        return ptr;
+    }
+};
--- SecondPass/README.md
+++ SecondPass/README.md
@@ -25,3 +25,4 @@ ID | Name | Difficulty | Category
  ---- | ------------- | -------- | ------
 203|[Remove Linked List Elements](https://leetcode.com/problems/remove-linked-list-elements/)| Easy | Deletion
 707|[Design Linked List](https://leetcode.com/problems/design-linked-list/)| Medium | Design
+206|[Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/)| Easy | List manipulation
