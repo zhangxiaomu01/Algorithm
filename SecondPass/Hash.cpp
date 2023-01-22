2b14bb0ffc30259c18fc2984f6c8cd03153fb1b1
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2023-01-21 21:49:00 -0800

    Adds problem 383 to the list

--- SecondPass/Hash.cpp
+++ SecondPass/Hash.cpp
@@ -440,3 +440,51 @@ public:
         return result;
     }
 };
+
+ /*
+    383. Ransom Note
+    https://leetcode.com/problems/ransom-note/
+
+    Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+    Each letter in magazine can only be used once in ransomNote.
+
+    Example 1:
+    Input: ransomNote = "a", magazine = "b"
+    Output: false
+
+    Example 2:
+    Input: ransomNote = "aa", magazine = "ab"
+    Output: false
+
+    Example 3:
+    Input: ransomNote = "aa", magazine = "aab"
+    Output: true
+    
+
+    Constraints:
+    1 <= ransomNote.length, magazine.length <= 105
+    ransomNote and magazine consist of lowercase English letters.
+ */
+// Hash map： note for special case (e.g. value only consists lower case characters), we can optimize the
+// performance by using an array.
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int record[26] = {0};
+        //add
+        if (ransomNote.size() > magazine.size()) {
+            return false;
+        }
+        for (int i = 0; i < magazine.length(); i++) {
+            record[magazine[i]-'a'] ++;
+        }
+        for (int j = 0; j < ransomNote.length(); j++) {
+            record[ransomNote[j]-'a']--;
+            if(record[ransomNote[j]-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
