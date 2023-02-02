c250eb6491b3ebbb4b298f7a4e25713366e1869b
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2023-02-01 23:48:45 -0800

    Adds problem 347 to the list.

--- SecondPass/README.md
+++ SecondPass/README.md
@@ -64,3 +64,4 @@ ID | Name | Difficulty | Category
 1047|[Remove All Adjacent Duplicates In String](https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/)| Easy | Stack
 150|[Evaluate Reverse Polish Notation](https://leetcode.com/problems/evaluate-reverse-polish-notation/)| Medium | Stack
 239|[Sliding Window Maximum](https://leetcode.com/problems/sliding-window-maximum/)| hard | maximum queue
+347|[Top K Frequent Elements](https://leetcode.com/problems/top-k-frequent-elements/)| Medium | priority queue
--- SecondPass/StackQueue.cpp
+++ SecondPass/StackQueue.cpp
@@ -458,3 +458,58 @@ public:
     }
 };
 
+/*
+    347. Top K Frequent Elements
+    https://leetcode.com/problems/top-k-frequent-elements/
+ 
+    Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+    Example 1:
+    Input: nums = [1,1,1,2,2,3], k = 2
+    Output: [1,2]
+
+    Example 2:
+    Input: nums = [1], k = 1
+    Output: [1]
+    
+
+    Constraints:
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+    k is in the range [1, the number of unique elements in the array].
+    It is guaranteed that the answer is unique.
+    
+
+    Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ */
+class Solution {
+private:
+    // maintain the minimum priority queue.
+    struct comp {
+        public:
+            bool operator()(pair<int, int>& l, pair<int, int>& r) {
+                return l.second > r.second;
+            }
+    };
+
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> uMap;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq;
+        vector<int> res;
+        for (int i : nums) uMap[i] ++;
+
+        for (auto i = uMap.begin(); i != uMap.end(); ++i) {
+            pq.push( {i->first, i->second} );
+            if (pq.size() > k) pq.pop();
+        }
+
+        for (int i = 0; i < k; ++i) {
+            res.push_back(pq.top().first);
+            pq.pop();
+        }
+
+        return res;
+    }
+};
+
