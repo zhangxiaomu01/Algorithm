52f5205a79ec16bb48a2e0b5d136b2dd2a9c0698
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2023-02-19 21:40:13 -0800

    Adds problem 222 to the list.

--- SecondPass/README.md
+++ SecondPass/README.md
@@ -79,4 +79,5 @@ ID | Name | Difficulty | Category
 572|[Subtree of Another Tree](https://leetcode.com/problems/subtree-of-another-tree/)| Easy | recursive / serialize Tree / tree hash
 104|[Maximum Depth of Binary Tree](https://leetcode.com/problems/maximum-depth-of-binary-tree/)| Easy | iterative / recursive
 111|[Minimum Depth of Binary Tree](https://leetcode.com/problems/minimum-depth-of-binary-tree/)| Easy | iterative / recursive
+222|[Count Complete Tree Nodes](https://leetcode.com/problems/count-complete-tree-nodes/)| Medium | complete binary tree
 
--- SecondPass/Tree.cpp
+++ SecondPass/Tree.cpp
@@ -778,3 +778,64 @@ public:
         return res;
     }
 };
+
+ /*
+    222. Count Complete Tree Nodes
+    https://leetcode.com/problems/count-complete-tree-nodes/
+    Given the root of a complete binary tree, return the number of the nodes in the tree.
+    According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+    Design an algorithm that runs in less than O(n) time complexity.
+
+    
+
+    Example 1:
+    Input: root = [1,2,3,4,5,6]
+    Output: 6
+
+    Example 2:
+    Input: root = []
+    Output: 0
+
+    Example 3:
+    Input: root = [1]
+    Output: 1
+
+    Constraints:
+    The number of nodes in the tree is in the range [0, 5 * 104].
+    0 <= Node.val <= 5 * 104
+    The tree is guaranteed to be complete.
+ */
+ // For this impl, the complexity is O(lgn * lgn).
+ // The key insight is to detect the complete and full sub trees. O(n) solution is trivial, omit here.
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
+
+        int leftDepth = 0;
+        int rightDepth = 0;
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        while (left) {
+            left = left->left;
+            leftDepth ++;
+        }
+
+        while(right) {
+            right = right->right;
+            rightDepth ++;
+        }
+
+        // Note for a complete binary tree, if left most depth == right most depth, then 
+        // it is a complete and full binary tree, we can easily calculate the node number
+        // with 2^height - 1;
+        if (leftDepth == rightDepth) {
+            // Equivalent to 2 << leftDepth - 1;
+            return pow(2, leftDepth + 1) - 1;
+        }
+
+        // We only do the search if we found out that the sub tree is not a complete full tree.
+        // Which roughly will discard about half of the nodes after each search!
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
