53f668c89af134a96db2f0e5019ed4747f8e0aaa
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2023-02-25 17:11:48 -0800

    Adds problem 105 to the list.

--- SecondPass/README.md
+++ SecondPass/README.md
@@ -87,4 +87,4 @@ ID | Name | Difficulty | Category
 112|[Path Sum](https://leetcode.com/problems/path-sum/)| Easy | Iterative / Recursive
 113|[Path Sum II](https://leetcode.com/problems/path-sum-ii/)| Medium | Iterative / Recursive
 106|[**Construct Binary Tree from Inorder and Postorder Traversal**](https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/)| Medium | Recursive
-
+105|[**Construct Binary Tree from Preorder and Inorder Traversal**](https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/)| Medium | Recursive
--- SecondPass/Tree.cpp
+++ SecondPass/Tree.cpp
@@ -1441,8 +1441,6 @@ private:
             }
         }
         int newPostOrderEnd = postOrderStart + (newInorderEnd - inorderStart);
-        // cout << "newInorderEnd: " << newInorderEnd << endl;
-        // cout << "newPostOrderEnd: " << newPostOrderEnd << endl;
         cur->left = construct(inorder, inorderStart, newInorderEnd, postOrder, postOrderStart, newPostOrderEnd);
         // Note we need to exclude the last element from postOrder array!
         // We need to make sure the new post order start is exacly the same as `newPostOrderEnd`, because in postorder
@@ -1454,6 +1452,57 @@ private:
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         return construct(inorder, 0, inorder.size(), postorder, 0, postorder.size());
+    }
+};
+
+ /*
+    105. Construct Binary Tree from Preorder and Inorder Traversal
+    https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+    Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary
+    tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+ 
+    Example 1:
+    Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+    Output: [3,9,20,null,null,15,7]
+
+    Example 2:
+    Input: preorder = [-1], inorder = [-1]
+    Output: [-1]
+    
+
+    Constraints:
+    1 <= preorder.length <= 3000
+    inorder.length == preorder.length
+    -3000 <= preorder[i], inorder[i] <= 3000
+    preorder and inorder consist of unique values.
+    Each value of inorder also appears in preorder.
+    preorder is guaranteed to be the preorder traversal of the tree.
+    inorder is guaranteed to be the inorder traversal of the tree.
+ */
+// Recursive
+class Solution {
+private:
+    TreeNode* construct(vector<int>& inorder, int inorderStart, int inorderEnd, vector<int>& preorder, int preorderStart, int preorderEnd) {
+        if (preorderStart >= preorderEnd) return nullptr;
+        
+        TreeNode* cur = new TreeNode(preorder[preorderStart]);
+
+        int newInorderEnd = 0;
+        for (int i = inorderStart; i < inorderEnd; ++i) {
+            if (inorder[i] == preorder[preorderStart]) {
+                newInorderEnd = i;
+                break;
+            }
+        }
+        int newPreorderEnd = preorderStart + 1 + (newInorderEnd - inorderStart);
+        // Needs to exclude the left most node in preorder array.
+        cur->left = construct(inorder, inorderStart, newInorderEnd, preorder, preorderStart + 1, newPreorderEnd);
+        cur->right = construct(inorder, newInorderEnd + 1, inorderEnd, preorder, newPreorderEnd, preorderEnd);
+        return cur;
+    }
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return construct(inorder, 0, inorder.size(), preorder, 0, preorder.size());
     }
 };
 
