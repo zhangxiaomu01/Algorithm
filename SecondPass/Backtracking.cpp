664381c9f46aa48acc15b0dc0c3772277f4c1e2b
Jun Zhang <zhangxiaomu01@gmail.com>
2023-06-14 10:51:39 +0800

    Adds problem 46 to the list.

--- SecondPass/Backtracking.cpp
+++ SecondPass/Backtracking.cpp
@@ -788,3 +788,58 @@ public:
         return res;
     }
 };
+
+ /*
+    46. Permutations
+    https://leetcode.com/problems/permutations/
+    Given an array nums of distinct integers, return all the possible permutations.
+    You can return the answer in any order.
+
+    
+
+    Example 1:
+    Input: nums = [1,2,3]
+    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+    Example 2:
+    Input: nums = [0,1]
+    Output: [[0,1],[1,0]]
+
+    Example 3:
+    Input: nums = [1]
+    Output: [[1]]
+    
+
+    Constraints:
+    1 <= nums.length <= 6
+    -10 <= nums[i] <= 10
+    All the integers of nums are unique.
+ */
+class Solution {
+private:
+    void backtracking(vector<int>& nums, vector<vector<int>>& res, vector<int>& perm, vector<int>& used) {
+        if (perm.size() == nums.size()) {
+            res.push_back(perm);
+            return;
+        }
+        
+        for(int i = 0; i < nums.size(); ++i) {
+            if (used[nums[i] + 10] == 1) continue;
+            used[nums[i] + 10] = 1;
+            perm.push_back(nums[i]);
+            backtracking(nums, res, perm, used);
+            perm.pop_back();
+            used[nums[i] + 10] = 0;
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> perm;
+        vector<int> used(21, 0);
+        backtracking(nums, res, perm, used);
+        return res;
+    }
+};
+
+
--- SecondPass/README.md
+++ SecondPass/README.md
@@ -114,4 +114,5 @@ ID | Name | Difficulty | Category
 93|[**Restore IP Addresses**](https://leetcode.com/problems/restore-ip-addresses/)| Medium | Backtracking - not easy to impl
 78|[Subsets](https://leetcode.com/problems/subsets/)| Medium | Backtracking + simulation
 90|[Subsets II](https://leetcode.com/problems/subsets-ii/)| Medium | Backtracking + simulation
-491|[Non-decreasing Subsequences](https://leetcode.com/problems/non-decreasing-subsequences/)| Medium | Backtracking
+491|[**Non-decreasing Subsequences**](https://leetcode.com/problems/non-decreasing-subsequences/)| Medium | Backtracking
+46|[Permutations](https://leetcode.com/problems/permutations/)| Medium | Backtracking
