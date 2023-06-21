61dd1df1a44c20867e27674102ea70c6ee60d01a
Jun Zhang <zhangxiaomu01@gmail.com>
2023-06-21 17:04:44 +0800

    Adds problem 416 to the list.

--- SecondPass/DP.cpp
+++ SecondPass/DP.cpp
@@ -465,3 +465,67 @@ public:
         return res;
     }
 };
+
+ /*
+    416. Partition Equal Subset Sum
+    https://leetcode.com/problems/partition-equal-subset-sum/
+    Given an integer array nums, return true if you can partition the array into two subsets 
+    such that the sum of the elements in both subsets is equal or false otherwise.
+
+
+    Example 1:
+    Input: nums = [1,5,11,5]
+    Output: true
+    Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+    Example 2:
+    Input: nums = [1,2,3,5]
+    Output: false
+    Explanation: The array cannot be partitioned into equal sum subsets.
+    
+
+    Constraints:
+    1 <= nums.length <= 200
+    1 <= nums[i] <= 100
+ */
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int e : nums) sum += e;
+        if (sum % 2 == 1) return false;
+        int target = sum / 2;
+        vector<vector<bool>> dp(nums.size() + 1, vector<bool>(target + 1, false));
+        dp[0][0] = true;
+
+        for (int i = 1; i <= nums.size(); ++i) {
+            for (int j = target; j >= 0; --j) {
+                if (j < nums[i-1]) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+            }
+        }
+        return dp[nums.size()][target];
+    }
+};
+
+// Optimized version
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int e : nums) sum += e;
+        if (sum % 2 == 1) return false;
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= nums.size(); ++i) {
+            // We needs info from dp[i-1][j] & dp[i-1][j-nums[i-1]], thus we need to iterate from
+            // the back.
+            for (int j = target; j >= nums[i-1]; --j) {
+                dp[j] = dp[j-nums[i-1]] || dp[j];
+            }
+        }
+        return dp[target];
+    }
+};
