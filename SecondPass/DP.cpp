34412c20d9d9b418dc62b186848b8417a96ca71c
Jun Zhang <zhangxiaomu01@gmail.com>
2023-07-05 15:21:55 +0800

    Adds problem 718 to the list.

--- SecondPass/DP.cpp
+++ SecondPass/DP.cpp
@@ -1925,6 +1925,67 @@ public:
     }
 };
 
+ /*
+    718. Maximum Length of Repeated Subarray
+    https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+    Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears 
+    in both arrays.
+
+
+    Example 1:
+    Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+    Output: 3
+    Explanation: The repeated subarray with maximum length is [3,2,1].
+
+    Example 2:
+    Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+    Output: 5
+    Explanation: The repeated subarray with maximum length is [0,0,0,0,0].
+    
+
+    Constraints:
+    1 <= nums1.length, nums2.length <= 1000
+    0 <= nums1[i], nums2[i] <= 100
+ */
+// DP
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        int res = 0;
+        for(int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    res = max(dp[i][j], res);
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
+// DP optimized space!
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        vector<int> dp(n+1, 0);
+        int res = 0;
+        for(int i = 1; i <= m; ++i) {
+            for (int j = n; j >= 1; --j) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    dp[j] = dp[j-1] + 1;
+                    res = max(dp[j], res);
+                } else dp[j] = 0; // For 1d table, we need to reset value!
+            }
+        }
+
+        return res;
+    }
+};
 
  /*
     300. Longest Increasing Subsequence
