e0f3962dedd9902bd12358cdcd753ff729f8c12e
Jun Zhang <zhangxiaomu01@gmail.com>
2023-07-09 15:58:12 +0800

    Adds problem 53 to the list.

--- SecondPass/DP.cpp
+++ SecondPass/DP.cpp
@@ -2155,3 +2155,62 @@ public:
         return res;
     }
 };
+
+ /*
+    53. Maximum Subarray
+    https://leetcode.com/problems/maximum-subarray/
+    Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+    
+
+    Example 1:
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+    Example 2:
+    Input: nums = [1]
+    Output: 1
+    Explanation: The subarray [1] has the largest sum 1.
+
+    Example 3:
+    Input: nums = [5,4,-1,7,8]
+    Output: 23
+    Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+    
+
+    Constraints:
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+ */
+// DP:
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = INT_MIN;
+        int n = nums.size();
+        vector<int> dp(n+1, 0);
+        for (int i = 1; i <= n; ++i) {
+            // Similar idea as greedy
+            dp[i] = max(dp[i-1] + nums[i-1], nums[i-1]);
+            res = max(res, dp[i]);
+        }
+
+        return res;
+    }
+};
+
+// Greedy
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = INT_MIN;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            res= max(res, sum);
+            if (sum < 0) sum = 0;
+        }
+        return res;
+    }
+};
--- SecondPass/README.md
+++ SecondPass/README.md
@@ -157,6 +157,7 @@ ID | Name | Difficulty | Category
 300|[Longest Increasing Subsequence](https://leetcode.com/problems/longest-increasing-subsequence/)| Medium | DP + O(nlogn) solution
 674|[Longest Continuous Increasing Subsequence](https://leetcode.com/problems/longest-continuous-increasing-subsequence/)| Easy | DP + Greedy
 1035|[Uncrossed Lines](https://leetcode.com/problems/uncrossed-lines/)| Medium | DP / longest common subsequence
+53|[Maximum Subarray](https://leetcode.com/problems/maximum-subarray/)| Medium | DP / Greedy
 
 
 
