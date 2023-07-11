c89d9e98b4dc4bdd15e2f907fe68d185db38316b
Jun Zhang <zhangxiaomu01@gmail.com>
2023-07-11 11:38:59 +0800

    Adds problem 647 to the list.

--- SecondPass/DP.cpp
+++ SecondPass/DP.cpp
@@ -2429,3 +2429,56 @@ public:
         return pre[n];
     }
 };
+
+ /*
+    647. Palindromic Substrings
+    https://leetcode.com/problems/palindromic-substrings/
+    Given a string s, return the number of palindromic substrings in it.
+    A string is a palindrome when it reads the same backward as forward.
+    A substring is a contiguous sequence of characters within the string.
+
+    
+
+    Example 1:
+    Input: s = "abc"
+    Output: 3
+    Explanation: Three palindromic strings: "a", "b", "c".
+
+    Example 2:
+    Input: s = "aaa"
+    Output: 6
+    Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+    
+
+    Constraints:
+    1 <= s.length <= 1000
+    s consists of lowercase English letters.
+ */
+// DP:
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        // dp[i][j] means whether the s[i..j] is the palindrome. If it is, then we only 
+        // need to check s[i-1] == s[j+1]. Please note that typically I like to initialize
+        // dp to be n+1 size, for this problem, using n size is easier, given i..j are both
+        // inclusive!
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int res = 0;
+        for (int i = 0; i < n; ++i) dp[i][i] = true;
+
+        for (int i = n-1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (s[i] == s[j]) {
+                    // if s[i] == s[j], then we know that 'a' or 'aa' are both valid!
+                    // j-i <= 1 check is necessary for avoiding above corner cases.
+                    if (j - i <= 1 || dp[i+1][j-1]) {
+                        dp[i][j] = true; 
+                        res++;
+                    } 
+                }
+            }
+        }
+        return res;
+    }
+};
