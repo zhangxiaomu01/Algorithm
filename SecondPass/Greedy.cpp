39778b8935d4d37c732a8065685695a35d8e5af6
Jun Zhang <zhangxiaomu01@gmail.com>
2023-07-21 14:26:34 +0800

    Adds problem 45 to the list.

--- SecondPass/Greedy.cpp
+++ SecondPass/Greedy.cpp
@@ -153,3 +153,56 @@ public:
         return maxRange >= n-1;
     }
 };
+
+ /*
+    45. Jump Game II
+    https://leetcode.com/problems/jump-game-ii/
+    You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+    Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], 
+    you can jump to any nums[i + j] where:
+    0 <= j <= nums[i] and
+    i + j < n
+    Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+    
+
+    Example 1:
+    Input: nums = [2,3,1,1,4]
+    Output: 2
+    Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the 
+    last index.
+
+    Example 2:
+    Input: nums = [2,3,0,1,4]
+    Output: 2
+    
+
+    Constraints:
+    1 <= nums.length <= 10^4
+    0 <= nums[i] <= 1000
+    It's guaranteed that you can reach nums[n - 1].
+ */
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        // We only increase the # of steps if we reaches the current max range
+        int curMaxRange = 0;
+        int nextMaxRange = 0;
+        int res = 0;
+        if (nums.size() <= 1) return 0;
+        for (int i = 0; i < n; ++i) {   
+            if (i > nextMaxRange) return 0;
+            nextMaxRange = max(nextMaxRange, nums[i] + i);
+            // We reached the maximum range we can currently reach, starting with 0
+            if (i == curMaxRange) {
+                res++;
+                // Record the next step that the distance we can reach.
+                curMaxRange = nextMaxRange;
+                if (curMaxRange >= n-1) return res;
+            }
+        } 
+        return res;
+    }
+};
--- SecondPass/README.md
+++ SecondPass/README.md
@@ -198,3 +198,4 @@ ID | Name | Difficulty | Category
 455|[Assign Cookies](https://leetcode.com/problems/assign-cookies/)| Easy | Greedy
 376|[**Wiggle Subsequence**](https://leetcode.com/problems/wiggle-subsequence/)| Medium | Greedy
 55|[Jump Game](https://leetcode.com/problems/jump-game/)| Medium | Greedy
+45|[**Jump Game II**](https://leetcode.com/problems/jump-game-ii/)| Medium | Greedy
