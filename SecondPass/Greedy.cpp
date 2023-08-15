a6b72112bf9c90c0bb0e7378f60a7f39cb74dc75
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2023-08-15 21:28:31 +0800

    Small tweaks of the file.

--- SecondPass/Greedy.cpp
+++ SecondPass/Greedy.cpp
@@ -667,7 +667,6 @@ public:
     https://leetcode.com/problems/monotone-increasing-digits/
     An integer has monotone increasing digits if and only if each pair of adjacent digits x and y 
     satisfy x <= y.
-
     Given an integer n, return the largest number that is less than or equal to n with monotone 
     increasing digits.
 
